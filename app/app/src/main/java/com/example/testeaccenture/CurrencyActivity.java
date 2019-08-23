5169a4439f0738b143cf40754bf5fcf296c2c30e
mayarals <limas.mayara@gmail.com>
2019-08-23 14:45:47 -0300

    validacao email e senha

--- app/app/src/main/java/com/example/testeaccenture/CurrencyActivity.java
+++ app/app/src/main/java/com/example/testeaccenture/CurrencyActivity.java
@@ -3,12 +3,25 @@ package com.example.testeaccenture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CurrencyActivity extends AppCompatActivity {
+
+    private TextView textViewNomeUsuario;
+    private TextView textViewNumeroConta;
+    private TextView textViewValorSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency);
+
+        initViews();
+    }
+
+    public void initViews(){
+        textViewNomeUsuario = findViewById(R.id.txtNomeUsuario);
+        textViewNumeroConta = findViewById(R.id.txtNumeroConta);
+        textViewValorSaldo = findViewById(R.id.txtValorSaldo);
     }
 }
--- app/app/src/main/java/com/example/testeaccenture/LoginActivity.java
+++ app/app/src/main/java/com/example/testeaccenture/LoginActivity.java
@@ -2,13 +2,58 @@ package com.example.testeaccenture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputLayoutEmail;
+    private  TextInputLayout textInputLayoutPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateLogin();
+            }
+        });
     }
+
+    private void validateLogin() {
+        String email = textInputLayoutEmail.getEditText().getText().toString();
+        String password = textInputLayoutPassword.getEditText().getText().toString();
+
+        if (email.isEmpty()) {
+            textInputLayoutEmail.setError("Insira um endereço de email");
+            return;
+        }
+
+        if (password.isEmpty()) {
+            textInputLayoutPassword.setError("Insira uma senha");
+            return;
+        }
+
+        Intent intent = new Intent(LoginActivity.this, CurrencyActivity.class);
+
+        startActivity(intent);
+    }
+
+    private void initViews(){
+        textInputLayoutEmail = findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = findViewById(R.id.textInputLayoutPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
 }
